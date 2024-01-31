c380c4987da4290a68fa1367c430c8302d1e1bad
Sasha <georgefbrockman@gmail.com>
2024-01-31 09:42:42 -0600

    adds skeleton of methods to Branch class

---
+++ lib/models/branch.py
@@ -0,0 +1,46 @@
+# lib/models/branch.py
+from models.__init__ import CURSOR, CONN
+
+class Branch:
+
+    all = {}
+
+    def __init__(self, name, address):
+        pass
+
+    @classmethod
+    def create(cls, name, address):
+        pass
+
+    def delete(self):
+        pass
+
+    @classmethod
+    def get_all(cls):
+        pass
+    
+    @classmethod
+    def instance_from_db(cls, row):
+        pass
+
+    def save(self):
+        pass
+
+    @property
+    def name(self):
+        pass
+
+    @name.setter
+    def name(self, name):
+        pass
+
+    @property
+    def address(self):
+        pass
+
+    @address.setter
+    def address(self, address):
+        pass
+
+    def customers(self):
+        pass
