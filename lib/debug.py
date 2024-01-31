2c545e7702037e246f032f211d7895978178a0cf
Sasha <georgefbrockman@gmail.com>
2024-01-31 12:47:55 -0600

    set up pytest and a single test file

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,18 @@
 
 from models.__init__ import CONN, CURSOR
 import ipdb
+from models.branch import Branch
 
+def debug_database():
+    Branch.drop_table()
+    Branch.create_table()
+
+    hoover = Branch.create("Hoover", "1558_Valleydale_Rd")
+    homewood = Branch.create("Homewood", "2865_Lakeshore_Dr")
+    trussville = Branch.create("Trussville", "8456_Carraway_St")
+
+debug_database()
+
+test = Branch.find_by_id(2)
 
 ipdb.set_trace()
--- lib/seed.py
+++ lib/seed.py
@@ -1,14 +1,3 @@
 # lib/seed.py
 
 from models.__init__ import CURSOR, CONN
-from models.branch import Branch
-
-def seed_database():
-    Branch.drop_table()
-    Branch.create_table()
-
-    hoover = Branch.create("Hoover", "1558_Valleydale_Rd")
-    homewood = Branch.create("Homewood", "2865_Lakeshore_Dr")
-    trussville = Branch.create("Trussville", "8456_Carraway_St")
-
-seed_database()
