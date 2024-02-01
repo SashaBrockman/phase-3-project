c90719799c656ad0876163908d0fc7973aea3509
Sasha <georgefbrockman@gmail.com>
2024-02-01 09:22:57 -0600

    sets up getters and setters for Customer

--- lib/models/customer.py
+++ lib/models/customer.py
@@ -6,39 +6,54 @@ class Customer:
     all = {}
 
     def __init__(self, name, account_number, balance = 0):
-        pass
+        self.id = None
+        self.name = name
+        self.account_number = account_number
+        self.balance = balance
 
     @property
     def name(self):
-        pass
+        return self._name
 
     @name.setter
     def name(self, name):
-        pass
+        if (type(name) == str) and (name != ""):
+            self._name = name
+        else:
+            raise ValueError("Name must be a non empty string.")
 
     @property
     def account_number(self):
-        pass
+        return self._account_number
 
     @account_number.setter
     def account_number(self, account_number):
-        pass
+        if (type(account_number) = int) and (len(account_number) != 0):
+            self._account_number = account_number
+        else:
+            raise ValueError("Account number must be a non empty integer")
 
     @property
     def balance(self):
-        pass
+        return self._balance
 
     @balance.setter
-    def balance(self):
-        pass
+    def balance(self, balance):
+        if (type(balance) = float) and (len(balance) != 0):
+            self._balance = balance
+        else:
+            raise ValueError("Balance must be a non empty float")
 
     @property
     def branch_id(self):
-        pass
+        return self._branch_id
 
     @branch_id.setter
     def branch_id(self, id):
-        pass
+        if (type(branch_id) = int) and (len(branch_id) != 0):
+            self._branch_id = branch_id
+        else:
+            raise ValueError("Branch id must be a non empty integer")
 
     @classmethod
     def create_table(cls):
@@ -76,6 +91,9 @@ class Customer:
 
     @classmethod
     def has_account_number(cls, account_number):
+        pass
+
+    def add_to_balance(self, amount):
         pass
 
     def branch(self):
