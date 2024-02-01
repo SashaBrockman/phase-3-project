cb31b62a7cc96142b98d1ff7e781f155ea99c482
Sasha <georgefbrockman@gmail.com>
2024-02-01 14:59:51 -0600

    sets up skeleton of customer_menu

--- lib/customer_helpers.py
+++ lib/customer_helpers.py
@@ -3,4 +3,31 @@
 from models.customer import Customer
 
 def customer_cli():
-    print("Performing useful function#1.")
+    while True:
+        customer_menu()
+        customer_command = input("> ")
+        if customer_command == "0":
+            break
+        elif customer_command == "1":
+            pass
+        elif customer_command == "2":
+            pass
+        elif customer_command == "3":
+            pass
+        elif customer_command == "4":
+            pass
+        elif customer_command == "5":
+            pass
+        else:
+            print("Please enter a valid number command.")
+
+
+def customer_menu():
+    print("You have reached the Customer menu.")
+    print("Please select an option: ")
+    print("0: Return to previous menu")
+    print("1: Display all customers")
+    print("2: Display customer by account number")
+    print("3: Display branch information for a customer")
+    print("4: Create a customer")
+    print("5: Delete a customer")
