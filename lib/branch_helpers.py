254f6aaffc7ce63dca755b462c1a275cf8cef6ed
Sasha <georgefbrockman@gmail.com>
2024-02-04 10:17:09 -0600

    adjusts menu display formating

--- lib/branch_helpers.py
+++ lib/branch_helpers.py
@@ -4,8 +4,8 @@ from models.branch import Branch
 from customer_helpers import display_customer
 
 def branch_menu():
-    print("You have reached the branch menu.")
-    print("Please select an option: ")
+    print("\n -----BRANCH MENU----- \n")
+    print("Please select an option: \n")
     print("0: Return to previous menu")
     print("1: Display all Branches")
     print("2: Display customers for a Branch")
@@ -21,8 +21,10 @@ def branch_cli():
         if branch_command == "1":
             display_branches()
         elif branch_command == "2":
+            print("\n ---CUSTOMERS---")
             get_branch_by_name(display_customers)
         elif branch_command == "3":
+            print("\n ---TOTAL HELD---")
             get_branch_by_name(display_total)
         elif branch_command == "4":
             create_branch()
@@ -35,19 +37,19 @@ def branch_cli():
 
 
 def display_branches():
-    print("displaying branches...")
+    print("\n ---CURRENT BRANCHES--- \n")
     branches = Branch.get_all()
     
     for branch in branches:
         print(f"Branch name: {branch.name}, Branch address: {branch.address}")
     
-    input("Press Enter to return to previous menu")
+    input("\nPress Enter to return to previous menu \n")
 
 
 def get_branch_by_name(method):
     while True:
-        print("Enter 'cancel' at any time to return to previous menu")
-        print("Please enter the branch's name: ")
+        print("\nEnter 'cancel' at any time to return to previous menu")
+        print("Please enter the branch's name: \n")
         branch_name = input("> ")
         
         if branch_name == "cancel":
@@ -57,14 +59,15 @@ def get_branch_by_name(method):
             branch = Branch.find_by_name(branch_name)
             
             if branch:
+                print("/n")
                 method(branch)
-                input("Press enter to return to Branch menu.")
+                input("\nPress enter to return to Branch menu. \n")
                 break
             
             else:
-                print("There is no branch with that name!")
+                print("\nThere is no branch with that name! \n")
         else:
-            print("Branch name must be a non empty string!")
+            print("\nBranch name must be a non empty string! \n")
 
 def display_customers(branch):
     customers = branch.customers()
@@ -80,46 +83,46 @@ def display_total(branch):
     for customer in customers:
         total += customer.balance
     
-    print(f"The total held by this bank is ${total: .2f}.")
+    print(f"\nThe total held by this bank is ${total: .2f}.")
 
 
 def create_branch():
     looper = True
-    print("creating branch...")
+    print("\n ---CREATE BRANCH--- \n")
     
     while looper:
         print("Enter 'cancel' to leave branch creation.")
-        print("Please enter the branch name: ")
+        print("Please enter the branch name: \n")
         name = input("> ")
         
         if name == "cancel":
             looper = False
         
-        elif Branch.has_name(name):
-            print("A branch with that name already exists!")
+        elif (type(name) == str) and (Branch.has_name(name)):
+            print("\nA branch with that name already exists! \n")
         
         elif (type(name) == str) and (name != ""):
             while looper:
-                print("Please enter the branch's address: ")
+                print("\nPlease enter the branch's address: \n")
                 address = input("> ")
                 
                 if address == "cancel":
                     looper = False
                 
                 elif Branch.has_address(address):
-                    print("A branch with that address already exists!")
+                    print("\nA branch with that address already exists! \n")
                 
                 elif (type(address) == str) and (address != ""):
                     Branch.create(name, address)
-                    print("New branch has been created.")
+                    print("\nNew branch has been created.")
                     looper = False
                 
                 else:
-                    print("Incorrect format. Branch's address must be a non-empty string.")
+                    print("\nIncorrect format. Branch's address must be a non-empty string. \n")
         else:
-            print("Incorrect format. Branch name must be a non-empty string.")
+            print("\nIncorrect format. Branch name must be a non-empty string. \n")
 
 
 def delete_branch(branch):
     branch.delete()
-    print("Branch has been deleted.")
+    print("\nBranch has been deleted.")
--- lib/cli.py
+++ lib/cli.py
@@ -11,21 +11,22 @@ def main():
         menu()
         choice = input("> ")
         if choice == "0":
-            print("Goodbye!")
+            print("n\ Goodbye! \n")
             exit()
         elif choice == "1":
             branch_cli()
         elif choice == "2":
             customer_cli()
         else:
-            print("Please select a valid number option")
+            print("\n Please select a valid number option. \n")
 
 
 def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. View Branch menu")
-    print("2. View Customer menu")
+    print("\n -----WELCOME----- \n")
+    print("Please select an option: \n")
+    print("0: Exit the program")
+    print("1: View Branch menu")
+    print("2: View Customer menu")
 
 
 if __name__ == "__main__":
